180ea36846645a5fe3063be1f7bc34db57bf4cac
Diego Matheus <diegoamatheus@gmail.com>
2020-09-24 14:12:39 -0400

    File cleanup

--- src/database/connection.js
+++ src/database/connection.js
@@ -7,7 +7,7 @@ class Connection{
 
   Connect = async () => {
     try {
-      await Mongoose.connect("mongodb://localhost:27017/classTracker", {useNewUrlParser: true, useUnifiedTopology: true});
+      await Mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
       console.log("Mongoose connected");
     } catch (error) {
       throw new Error(error);
